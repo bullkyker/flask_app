4292de1065246eaa002b303726619672827ddc90
bullkyker <bullkyker@hotmail.com>
2016-09-09 11:58:17 -0400

    Missing a paranthesis

--- web.py
+++ web.py
@@ -20,7 +20,6 @@ import os
 #sys.path.append(root)
 from weather import get_location
 from flask import Flask, render_template, request
-from flaskrun import flaskrun
 app = None 
 app = Flask(__name__)
 @app.route("/")
@@ -38,7 +37,7 @@ def index():
 def about():
 	return render_template('about.html')
 
-if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000)
-    #app.run(debug = True, host='0.0.0.0', port=port)
-	flaskrun(app, 0.0.0.0, port)
+if __name__  ==  "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	#port = 5000
+	app.run(debug=True, host='0.0.0.0', port=port)	
