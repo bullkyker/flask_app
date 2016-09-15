11dcf265b05021af3ff3514de4511aea74c6a899
bullkyker <bullkyker@hotmail.com>
2016-09-15 08:05:47 -0400

    Fixed tip and changed font color

--- tip_bullkyker.py
+++ tip_bullkyker.py
@@ -20,26 +20,29 @@ def num(s):
         return 1.00
 Tips = []
 def suggest_tip(amount):
-	amount = num(amount)
+	#amount = num(amount)
+	#amount = 85.5
+	amount = float(amount)
 	fifteen = float(amount * 0.15) 
 	eighteen = float(amount * 0.18) 
 	twenty = float(amount * 0.2) 
 	Tips.append({
-		    "amount": str(amount), 
+		    "bill_amount": str(amount), 
 		     "tip_amount": str("%.2f" % round(fifteen,2)),
-			"percent": "fifteen percent"
+			"percent": " for fifteen percent"
 		})
 	Tips.append({
-		    "amount": str(amount), 
+		    "bill_amount": str(amount), 
 		     "tip_amount": str("%.2f" % round(eighteen,2)),
-			"percent": "eighteen percent"
+			"percent": " for eighteen percent"
 		})
 	Tips.append({
-		    "amount": str(amount), 
+		    "bill_amount": str(amount), 
 		     "tip_amount": str("%.2f" % round(twenty,2)),
-			"percent": "twenty percent"
+			"percent": " for twenty percent"
 		})
 	return Tips
-#print(suggest_tip(12.50))
+# mytip = suggest_tip(42.50)
+# print(mytip[0])
 	#return("The suggested tips for $%.2f are: 15 percent $%.2f, 18 percent $%.2f, 20 percent $%.2f" % (amount, fifteen, eighteen, twenty))
 #print("The totals would be: \n\t 15 percent $%.2f, \n\t 18 percent $%.2f, \n\t 20 percent $%.2f" % (fifteen + amount, eighteen + amount, twenty + amount))
