201d71b80d2ce126324af8d93edddd7ca48a128d
bullkyker <bullkyker@hotmail.com>
2016-09-16 10:19:08 -0400

    changed index

--- web.py
+++ web.py
@@ -14,6 +14,7 @@
 import sys
 import os
 import tip_bullkyker
+import amazon_scraper
 #from pathlib import Path # if you haven't already done so
 #root = str(Path(__file__).resolve().parents[1])
 # Or
